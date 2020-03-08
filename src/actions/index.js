2472ac70567c6a94936e725a52e71273ab71760a
Stephanie Jones <stephanie@Stephanies-MacBook-Pro-2.local>
2020-03-08 17:44:13 -0600

    Created fetchUser action and component; installed lodash; used memoize so only one fetch completed per user

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder'
+import _ from 'lodash'
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts')
@@ -8,4 +9,21 @@ export const fetchPosts = () => async dispatch => {
         payload: response.data
     })
 }
+
+export const fetchUser = (id) => dispatch => {
+    _fetchUser(id, dispatch)
+}
+//_ means its a private fn
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`)
+
+    dispatch({
+        type: 'FETCH_USER',
+        payload: response.data
+    })
+
+})
+
+
+
 
--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,1 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchUser } from '../actions'
+
+class UserHeader extends Component {
+    componentDidMount() {
+        this.props.fetchUser(this.props.id)
+    }
+
+    render() {
+        const { user } = this.props
+        if (!user) {
+            return null
+        }
+        return (
+            <div className="header">{user.name}</div>
+        )
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return { user: state.users.find(user => user.id === ownProps.id) }
+}
+
+export default connect(
+    mapStateToProps,
+    { fetchUser }
+)(UserHeader)
