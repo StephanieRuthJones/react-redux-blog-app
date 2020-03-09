3d941731320890e08a8020d160ffc9b9b3f545a3
Stephanie Jones <stephanie@Stephanies-MacBook-Pro-2.local>
2020-03-08 18:07:56 -0600

    created fetchPostsAndUsers in actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,17 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 import _ from 'lodash'
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    console.log('about to fetch posts')
+    await dispatch(fetchPosts())
+    //_.map goes through all posts and pull of only userId properties
+    //will return array of userIds
+    //_uniq findss only unique ids
+    //fetchUser with each id in array
+    const userIds = _.uniq(_.map(getState().posts, 'userId'))
+    userIds.forEach(id => dispatch(fetchUser(id)))
+}
+
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts')
 
@@ -10,19 +21,16 @@ export const fetchPosts = () => async dispatch => {
     })
 }
 
-export const fetchUser = (id) => dispatch => {
-    _fetchUser(id, dispatch)
-}
-//_ means its a private fn
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`)
 
     dispatch({
         type: 'FETCH_USER',
         payload: response.data
     })
+}
 
-})
+
 
 
 
